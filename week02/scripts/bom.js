e282a2562b05b81ffc7b27c4867f8127d965b457
dianMcdawn <patricio.a.henriquez>
2024-09-24 12:36:11 -0300

    sync

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,25 @@
+
+//Cosntant Variables
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('list');
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+
+button.addEventListener('click', function () {
+    // Code to execute when the button is clicked
+    if (input.value.trim() !== '') {
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+    } else {
+        input.focus();
+    }
+});
+
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.value = '';
+    input.focus();
+});
