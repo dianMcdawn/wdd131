8383764982b34695542943a3c670341d0bbe6b24
dianMcdawn <patricio.a.henriquez>
2024-10-15 15:40:59 -0300

    sync

--- scripts/review.js
+++ scripts/review.js
@@ -1,21 +1,20 @@
-// 1️⃣ Initialize display element variable
-const visitsDisplay = document.querySelector(".reviews");
-//const product = urlParams.get('product')
+// checking if a parameters arrives
+const urlParams = new URLSearchParams(window.location.search);
+const product = urlParams.get('product');
 
-// 2️⃣ Get the stored VALUE for the reviewsCount-ls KEY in localStorage if it exists. If the reviewsCount KEY is missing, then assign 0 to the reviewsCount variable.
+// Initialize display element variable
+const visitsDisplay = document.querySelector(".reviews");
+
+// Get the stored VALUE for the reviewsCount-ls KEY in localStorage if it exists. If the reviewsCount KEY is missing, then assign 0 to the reviewsCount variable.
 let reviewsCount = Number(window.localStorage.getItem("reviewsCount-ls")) || 0;
 
-// 3️⃣ Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
-if (reviewsCount !== 0) {
-    visitsDisplay.textContent = reviewsCount;
-} else {
-    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+if (product !== null) {
+    // If submit is success increment the number of reviews by one.
+    reviewsCount++;
 }
 
-// 4️⃣ increment the number of visits by one.
-reviewsCount++;
+// Show the reviews count on screen
+visitsDisplay.textContent = reviewsCount;
 
-// 5️⃣ store the new visit total into localStorage, key=reviewsCount-ls
+// Store the new visit total into localStorage, key=reviewsCount-ls
 localStorage.setItem("reviewsCount-ls", reviewsCount);
-
-// 💡A client can view the localStorage data using the Applications panel in the browsers's DevTools - check it out on any major site.
